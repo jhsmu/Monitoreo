25e6a8a42f0cbd9def255df99cd4d88c6fcb3cd9
Ingenireo Jhonatan  Mena <91585216+jhsmu@users.noreply.github.com>
2025-08-19 18:52:51 -0500

    Update check_mongo.py

--- check_mongo.py
+++ check_mongo.py
@@ -2,15 +2,13 @@ import os
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 
+# Leer MONGO_URI desde variables de entorno
 MONGO_URI = os.getenv("MONGO_URI")
-DB_NAME = os.getenv("DATABASE_NAME")
 
 try:
     client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
-    db = client[DB_NAME]
-    db.command("ping")  # consulta básica
+    client.admin.command("ping")  # comando para validar conexión
     print("✅ Conexión exitosa a MongoDB Atlas")
-    exit(0)
 except ConnectionFailure as e:
-    print("❌ Error de conexión:", e)
+    print("❌ Error de conexión a MongoDB Atlas:", e)
     exit(1)
